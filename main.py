b1abdf5e6207084b40c8b38ea184acd5252e2ec7
Tim <85653488+Ecume@users.noreply.github.com>
2023-05-14 17:17:09 -0400

    changed up the code and added more
    almost done, still waiting for access for the caffe network

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ neural_net = cv2.dnn.readNetFromCaffe(prototxt_neural_path, model_neural_path)
 while True:
     
     _, image = cap.read()
-    img_height, img_weight = image.shape[0], image.shape[1]
+    img_height, img_width = image.shape[0], image.shape[1]
     blob = cv2.dnn.blobFromImage(cv2.resize(image,(300, 300)), 0.007, (300, 300), 130)
 
     neural_net.setInput(blob)
@@ -38,11 +38,26 @@ while True:
 
     for i in range(detected_objects.shape[2]):
 
-        new_confidence = int(detected_objects[0][0][i][2])
+        new_confidence = (detected_objects[0][0][i][2])
 
         if new_confidence > min_confidence:
 
             img_classes_index = int(detected_objects[0, 0, i, 1])
+
+            upper_left_x = int(detected_objects[0, 0, i, 3] * img_width)
+            upper_left_y = int(detected_objects[0, 0, i, 4] * img_height)
+            lower_right_x = int(detected_objects[0, 0, i, 5] * img_width)
+            lower_right_y = int(detected_objects[0, 0, i, 6] * img_height)
+
+            prediction_text = f"{img_classes[img_classes_index]}: {new_confidence:.2f}%"
+            cv2.rectangle(image, (upper_left_x, upper_left_y), (lower_right_x, lower_right_y), colors[img_classes_index], 3)
+            cv2.putText(image, prediction_text, (upper_left_x, upper_left_y -15 if upper_left_y > 30 else upper_left_y + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors[img_classes_index], 2)
+        
+    cv2.imshow("Objected detected", image)
+    cv2.waitKey(5)
+
+    cv2.destroyAllWindows()
+    cap.release()
 
     
 
